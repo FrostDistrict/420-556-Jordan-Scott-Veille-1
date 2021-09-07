30e278baa2d6d0b85e46071262340cbf7cc863e7
Slim Crisp <jordan.scott1396@gmail.com>
2021-09-07 12:04:09 -0400

    Initialisation du backend

---
+++ src/test/java/com/scott/reserved/ReservedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scott.reserved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
