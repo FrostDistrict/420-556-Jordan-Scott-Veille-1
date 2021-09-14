202da5571865381bf877b93677d1e936b973f1e0
FrostDistrict <jordan.scott1396@gmail.com>
2021-09-14 11:38:25 -0400

    test

--- src/main/java/com/scott/reserved/ReservedApplication.java
+++ src/main/java/com/scott/reserved/ReservedApplication.java
@@ -1,11 +1,13 @@
 package com.scott.reserved;
 
+import com.scott.reserved.model.Restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = Restaurant.class)
 @ComponentScan(value = "com.scott.reserved.*")
 public class ReservedApplication {
 
