70e89397af0cd2af440d6f27953407700e3ce344
FrostDistrict <jordan.scott1396@gmail.com>
2021-09-20 22:35:45 -0400

    Initialisation du backend

--- src/main/java/com/scott/reserved/ReservedApplication.java
+++ src/main/java/com/scott/reserved/ReservedApplication.java
@@ -1,17 +1,41 @@
 package com.scott.reserved;
 
+import com.scott.reserved.model.Restaurant;
+import com.scott.reserved.repository.RestaurantRepository;
+import com.scott.reserved.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableMongoRepositories("com.scott.reserved.repository")
 @ComponentScan(value = "com.scott.reserved.*")
-public class ReservedApplication {
+public class ReservedApplication implements CommandLineRunner {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ReservedApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Restaurant rest1 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+        Restaurant rest2 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+        Restaurant rest3 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+        Restaurant rest4 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+        Restaurant rest5 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+        Restaurant rest6 = new Restaurant("toto", "restorante", "test", "test", "test@gmail.com", 2);
+
+        restaurantRepository.saveAll(Arrays.asList(rest1, rest2, rest3, rest4, rest5, rest6));
+    }
 }
--- src/main/java/com/scott/reserved/model/Restaurant.java
+++ src/main/java/com/scott/reserved/model/Restaurant.java
@@ -28,4 +28,17 @@ public class Restaurant extends BaseEntity {
     private List<Location> locations;
 
     private Schedule schedule;
+
+    public Restaurant(@Size(min = 3, max = 70) String name, @Size(min = 3, max = 250) String description, String phone1, String phone2, @Email String email, int capacity) {
+        this.name = name;
+        this.description = description;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.capacity = capacity;
+    }
+
+    public Restaurant() {
+
+    }
 }
